98a8823dd45e46c6ecc2daefb96910971f6eafe3
KseniyaOsadcha <theksenic@gmail.com>
2021-03-07 01:07:16 +0200

    Добавьте файлы проекта.

---
+++ Collection/CustomDictionary.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace Collection
+{
+    class CustomDictionary<TKey, TValue> : IDictionary<TKey, TValue>
+    {
+
+        private struct Element
+        {
+            public int hashCode;
+            public TKey key;
+            public TValue value;
+        }
+        private const int startCount = 8;
+        private int currentSize = startCount;
+        private int currentElement = 0;
+        private Element[] customDictionary = new Element[startCount];
+
+
+        public void Add(TKey key, TValue value)
+        {
+            // check if there is some place for new element
+            if (currentElement == currentSize)
+            {
+                // increase size
+                currentSize *= 2;
+                Array.Resize(ref customDictionary, currentSize);
+            }
+            Element e = new Element();
+            e.hashCode = key.GetHashCode();
+            e.key = key;
+            e.value = value;
+            customDictionary[currentElement++] = e;
+
+        }
+        public void Clear()
+        {
+            Array.Clear(customDictionary, 0, customDictionary.Length);
+            currentElement = 0;
+            currentSize = startCount;
+            Array.Resize(ref customDictionary, currentSize);
+
+        }
+
+        public bool ContainsKey(TKey key)
+        {
+            int hash = key.GetHashCode();
+            foreach (Element el in customDictionary)
+            {
+                if (hash == el.hashCode && object.Equals(key, el.key))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private int FindIndex(TKey key)
+        {
+            int hash = key.GetHashCode();
+            foreach (Element el in customDictionary)
+            {
+                if (hash == el.hashCode && object.Equals(key, el.key))
+                {
+                    return Array.IndexOf(customDictionary, el);
+                }
+            }
+            return -1;
+        }
+
+        public bool Remove(TKey key)
+        {
+            int index = FindIndex(key);
+            if (index >= 0)
+            {
+                customDictionary = customDictionary.Where((val, idx) => idx != index).ToArray();
+                currentElement--;
+                return true;
+            }
+            return false;
+        }
+
+        public TValue this[TKey key]
+        {
+            get
+            {
+                int index = FindIndex(key);
+                if (index >= 0) return customDictionary[index].value;
+                return default(TValue);
+            }
+            set {
+                int index = FindIndex(key);
+                if (index >= 0) customDictionary[index].value = value;
+            }
+        }
+
+        public ICollection<TKey> Keys => getAllKeys();
+        private ICollection<TKey> getAllKeys()
+        {
+            List<TKey> keys = new List<TKey>();
+
+            foreach (Element el in customDictionary)
+            {
+                keys.Add(el.key);
+            }
+
+            return keys;
+        }
+        public ICollection<TValue> Values => getAllValues();
+        private ICollection<TValue> getAllValues()
+        {
+            List<TValue> values = new List<TValue>();
+
+            foreach (Element el in customDictionary)
+            {
+                values.Add(el.value);
+            }
+
+            return values;
+        }
+
+        public int Count => currentElement;
+
+        public bool IsReadOnly => false;
+
+        public bool TryGetValue(TKey key, [MaybeNullWhen(false)] out TValue value)
+        {
+            int hash = key.GetHashCode();
+            foreach (Element el in customDictionary)
+            {
+                if (hash == el.hashCode && object.Equals(key, el.key))
+                {
+                    value = el.value;
+                    return true;
+                }
+            }
+            value = default(TValue);
+            return false;
+        }
+
+        // not ready
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            throw new NotImplementedException();
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Collection/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            CustomDictionary<string, string> dict1 = new CustomDictionary<string, string>();
+
+            dict1.Add("Spain", "Madrid");
+            dict1.Add("Serbia", "Belgrade");
+            dict1.Add("Slovakia", "Bratislava");
+            dict1.Add("Finland", "Helsinki");
+            dict1.Add("France", "Paris");
+            dict1.Add("Georgia", "Tbilisi");
+            dict1.Add("Greece", "Athens");
+
+            Console.WriteLine("Values: ");
+            foreach( string v in dict1.Values)
+            {
+                Console.WriteLine(v);
+
+            }
+            Console.WriteLine("Keys: ");
+            foreach (string k in dict1.Keys)
+            {
+                Console.WriteLine(k);
+
+            }
+            Console.WriteLine("Count: ");
+            Console.WriteLine(dict1.Count);
+
+            dict1.Remove("Finland");
+            Console.WriteLine("Values: ");
+            foreach (string v in dict1.Values)
+            {
+                Console.WriteLine(v);
+
+            }
+            Console.WriteLine("Keys: ");
+            foreach (string k in dict1.Keys)
+            {
+                Console.WriteLine(k);
+
+            }
+            Console.WriteLine("Count: ");
+            Console.WriteLine(dict1.Count);
+
+            Console.WriteLine("try to find Georgia: ");
+            string value;
+            dict1.TryGetValue("Georgia", out value);
+            Console.WriteLine(value);
+
+            Console.WriteLine("try to find smth: ");
+            string value2;
+            dict1.TryGetValue("smth", out value2);
+            Console.WriteLine(value2);
+
+            Console.WriteLine("is there Greece: ");
+            Console.WriteLine(dict1.ContainsKey("Greece"));
+
+            Console.WriteLine("get capital by key Spain: ");
+            Console.WriteLine(dict1["Spain"]);
+
+            Console.WriteLine("set capital by key Serbia: ");
+            dict1["Serbia"] = "Belgrade2";
+            Console.WriteLine(dict1["Serbia"]);
+
+            Console.WriteLine("Clearing dict...");
+            dict1.Clear();
+            Console.WriteLine("Count: ");
+            Console.WriteLine(dict1.Count);
+        }
+    }
+}
